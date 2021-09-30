e23cccaecd5fa6782baed4a89fea3c90c2479d81
Victor Santelé <Vic16@hotmail.be>
2021-09-30 22:27:51 +0200

    style consistency

--- sw.js
+++ sw.js
@@ -1,43 +1,44 @@
 var CACHE_NAME = 'IESN_IG_DA';
 var urlsToCache = [
-    "/da/",
-    "/da/styles/app.css",
-    "/da/styles/pcp-highlight.css",
-    "/da/styles/print.css",
-    "/da/styles/reset.css",
-    "/da/styles/toastr.min.css",
-    "/da/scripts/app.js",
-    "/da/scripts/PseudoCodeParser.js",
-    "/da/scripts/libraries/jquery-3.2.1.min.js",
-    "/da/scripts/libraries/toastr.min.js",
-    "https://use.fontawesome.com/ca53c7c19d.css",
-    "https://use.fontawesome.com/ca53c7c19d.js",
+    '/da/',
+    '/da/styles/app.css',
+    '/da/styles/pcp-highlight.css',
+    '/da/styles/print.css',
+    '/da/styles/reset.css',
+    '/da/styles/toastr.min.css',
+    '/da/scripts/app.js',
+    '/da/scripts/PseudoCodeParser.js',
+    '/da/scripts/libraries/jquery-3.2.1.min.js',
+    '/da/scripts/libraries/toastr.min.js',
+    'https://use.fontawesome.com/ca53c7c19d.css',
+    'https://use.fontawesome.com/ca53c7c19d.js',
 ];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', function(event) {
     // Perform install steps
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then(function (cache) {
+        .then(function(cache) {
             return cache.addAll(urlsToCache);
         })
     );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', function(event) {
     console.log('Service worker is activated!');
 });
 
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.open(CACHE_NAME).then(function (cache) {
-            return cache.match(event.request).then(function (response) {
-                var fetchPromise = fetch(event.request).then(function (networkResponse) {
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.match(event.request).then(function(response) {
+                var fetchPromise = fetch(event.request).then(function(networkResponse) {
                     if (networkResponse && networkResponse.status === 200 && networkResponse.type === 'basic' && networkResponse.url.startsWith('http')) {
                         cache.put(event.request, networkResponse.clone())
                     }
                 })
+
                 return response || fetchPromise;
             })
         })

f34dbc8916de188c751443d19e8285e1681bd434
Victor Santelé <Vic16@hotmail.be>
2021-10-04 20:43:37 +0200

    change strategy: Race Cache Network

--- sw.js
+++ sw.js
@@ -30,19 +30,36 @@ self.addEventListener('activate', function(event) {
     console.debug('Service worker is activated!');
 });
 
-
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.match(event.request).then(function(response) {
-                var fetchPromise = fetch(event.request).then(function(networkResponse) {
-                    if (networkResponse && networkResponse.status === 200 && networkResponse.type === 'basic' && networkResponse.url.startsWith('http')) {
-                        cache.put(event.request, networkResponse.clone())
-                    }
-                })
-
-                return response || fetchPromise;
-            })
-        })
-    );
+    if (event.request.method !== 'GET') return;
+    if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
+    event.respondWith(cacheNetworkRace(event));
 });
+
+async function cacheNetworkRace(event) {
+    try {
+        const cache = await caches.open(CACHE_NAME);
+        const response = await Promise.any([cacheFetch(cache, event.request), networkFetch(cache, event.request)]);
+        return response;
+    } catch (error) {
+        console.error("Not found in cache or network");
+    }
+}
+
+async function cacheFetch(cache, request) {
+    const response = await cache.match(request);
+    if (response) return response;
+    return Promise.reject('Request not cached');
+}
+
+async function networkFetch(cache, request) {
+    try {
+        const response = await fetch(request)
+        if (response && response.status === 200 && response.type === 'basic' && response.url.startsWith('https')) {
+            cache.put(request, response.clone());
+        }    
+        return response;
+    } catch (error) {
+        return Promise.reject('Failed to fetch')
+    }
+}
